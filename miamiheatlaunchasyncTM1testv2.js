ded5ad02054ca12762a92f605c734bb6b91385f0
wilanalytics22 <wilanalytics22@gmail.com>
2018-08-28 11:15:39 -0400

    Update miamiheatlaunchasyncTM1testv2.js

--- miamiheatlaunchasyncTM1testv2.js
+++ miamiheatlaunchasyncTM1testv2.js
@@ -2,3 +2,5 @@
     a="//assets.adobedtm.com/launch-EN12f1efde8d034e88b0a934cb4c8617c2.min.js";b=document;c="script";d=b.createElement(c);d.src=a;d.type="text/java"+c;d.async=true;
     a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
     })();
+
+console.log('tag fired 8.28.18 WC');
