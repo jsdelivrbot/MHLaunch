45aed870f9fdcac799db51f1395790432a796285
wilanalytics22 <wilanalytics22@gmail.com>
2018-09-24 11:30:29 -0400

    Create getparams-v1.js

---
+++ getparams-v1.js
@@ -0,0 +1,15 @@
+function getQueryParams(qs) {
+    qs = qs.replace(/\+/g, " ");
+    var params = {},
+        re = /[?&]?([^=]+)=([^&]*)/g,
+        tokens;
+ 
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+ 
+    return params;
+}
+
+console.log( getQueryParams(parent.document.location.search) );
