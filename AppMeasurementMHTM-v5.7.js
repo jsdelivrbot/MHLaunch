79f11e65b5e2451088b95d6e3e2d69d03bd7cdef
wilanalytics22 <wilanalytics22@gmail.com>
2018-09-26 14:55:37 -0400

    Update and rename AppMeasurementMHTM-v5.6.js to AppMeasurementMHTM-v5.7.js

--- AppMeasurementMHTM-v5.7.js
+++ AppMeasurementMHTM-v5.7.js
@@ -259,4 +259,6 @@ var test = window.setInterval(() => {
 
 s.pageName = typeof parent.frames.TM.Tracking.satellite.data.pageLevelData.pageDetail != "undefined" ? parent.frames.TM.Tracking.satellite.data.pageLevelData.pageDetail : "";
 console.log('new version 9.26.18 1:26 PM');
+g = typeof parent.frames.document.URL != "undefined" ? parent.frames.document.URL : "";
+r = typeof parent.document.referrer != "undefined" ? parent.document.referrer : "";
 s.t();
