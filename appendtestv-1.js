097517fc6c37e3abf2a43211629578f2b36932e1
wilanalytics22 <wilanalytics22@gmail.com>
2018-10-24 14:12:40 -0400

    Create appendtestv-1.js

---
+++ appendtestv-1.js
@@ -0,0 +1,8 @@
+//Adobe Launch Start
+var s = document.createElement("script");
+s.setAttribute("src", "https://assets.adobedtm.com/launch-EN12f1efde8d034e88b0a934cb4c8617c2.min.js");
+s.onload = function() {
+    //console.log('Launch Loaded');
+}
+document.head.appendChild(s);
+//End
